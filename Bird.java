f2733d4bf170a1890caa8bc9a83a7568062d8b9d
JohnWonne <153517077+JohnWonne@users.noreply.github.com>
2023-12-22 23:57:33 +0700

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -1,3 +1,10 @@
 public class Bird {
    public float x, y, vx, vy;
+    public static final int RAD = 25;
+    private Image img;
+
+public Bird() {
+    x = FlappyBird.WIDTH / 2;
+    y = FlappyBird.HEIGHT /2;
+       try {
 }

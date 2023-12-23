467c9891769b8a3845a746125b3c265d52a3ec68
JohnWonne <153517077+JohnWonne@users.noreply.github.com>
2023-12-23 21:31:29 +0700

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -6,4 +6,9 @@ public class Bird {
     public Bird() {
         x = FlappyBird.WIDTH / 2;
         y = FlappyBird.HEIGHT / 2;
-    }       
+        try {
+            img = ImageIO.read(new File("sticker,375x360.u2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
